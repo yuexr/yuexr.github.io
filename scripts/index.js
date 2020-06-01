4eb3ea02e5875c2b5a925f3b8e53f1cf7a666f49
yuexr <axyuezhang@outlook.com>
2020-06-01 07:28:36 -0400

    accordion leif

--- scripts/index.js
+++ scripts/index.js
@@ -47,3 +47,18 @@ $(document).ready(function(){
 $(function(){
   $("#nav-root").load("../acnh/0nav.html"); 
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
