b8d862cf1200c9d591b6e4cd1e1c3525d2e5cd62
yuexr <axyuezhang@outlook.com>
2020-06-05 05:55:07 -0400

    blue rose wip, minor garden layout changes :3

--- scripts/index.js
+++ scripts/index.js
@@ -72,3 +72,20 @@ $(document).ready(function(){
   }
 })
 
+
+
+//MODAL STUFF
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
